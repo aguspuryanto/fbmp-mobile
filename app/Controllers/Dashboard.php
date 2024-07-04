f014f830acb54cf70c2284e469b93d07f200a25e
Agus Puryanto <aguspuryanto@gmail.com>
2024-07-04 14:43:46 +0700

    update dashboard

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ProductModel;
 use App\Models\UserModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
@@ -10,11 +11,13 @@ class Dashboard extends BaseController
 {
     private $db;
     private $akun;
+    private $itemModel;
     
     public function __construct()
     {
         $this->db = \Config\Database::connect();
         $this->akun = new UserModel();
+        $this->itemModel = new ProductModel();
         helper(["url","form"]);
     }
 
@@ -27,7 +30,10 @@ class Dashboard extends BaseController
         }
 
         $data['title'] = 'Dashboard';
-        $data['stat'] = ['akun' => $this->akun->where('status', 'aktif')->countAllResults(), 'iklan' => 0];
+        $data['stat'] = [
+            'akun' => $this->akun->where('status', 'aktif')->countAllResults(),
+            'iklan' => $this->itemModel->where('status', 'publish')->countAllResults()
+        ];
 
         return view('admin/dashboard', $data);
     }
