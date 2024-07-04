052e805b08e21da3c829476c516538ce2b526fd8
Agus Puryanto <aguspuryanto@gmail.com>
2024-07-04 15:03:12 +0700

    update register

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -23,17 +23,24 @@ class Register extends BaseController
         //set rules validation form
         $rules = [
             'name'          => 'required|min_length[3]|max_length[20]',
+            'nowa'          => 'required|min_length[3]|max_length[20]',
             'email'         => 'required|min_length[6]|max_length[50]|valid_email|is_unique[users.email]',
             'password'      => 'required|min_length[6]|max_length[200]',
-            'confpassword'  => 'matches[password]'
+            'kota'          => 'required|min_length[3]|max_length[20]',
+            'paket'         => 'required',
+            // 'confpassword'  => 'matches[password]'
         ];
          
         if($this->validate($rules)){
             $model = new UserModel();
             $data = [
                 'name'     => $this->request->getVar('name'),
+                'nowa'     => $this->request->getVar('nowa'),
                 'email'    => $this->request->getVar('email'),
-                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+                'kota'     => $this->request->getVar('kota'),
+                'paket'    => $this->request->getVar('paket'),
+                'created_at' => date('Y-m-d H:i:s')
             ];
             $model->save($data);
             return redirect()->to('/login');
